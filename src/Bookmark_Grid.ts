9346e0296e91c45d27d7a65b1add82d80e220408
Dmitriy Narzyaev <nozaki@yandex.ru>
2026-03-06 22:12:33 +0300

    bookmark grid moved to a new file

---
+++ src/Bookmark_Grid.ts
@@ -0,0 +1,59 @@
+import Bookmark from "./Bookmark";
+import Global from "./Global";
+import Container = PIXI.Container;
+import "pixi.js";
+
+export default class Bookmark_Grid extends Container {
+    private _level:ILevel;
+
+    constructor(level:ILevel) {
+        super();
+            this._level = level;
+            this.createBookmarks();
+        }
+
+        private createBookmarks():void {
+        let bookmarkX: number = 0;
+        let bookmarkY: number = 0;
+        let colorRed: string = "10";
+        let colorGreen: string = "50";
+        let colorBlue: string = "90";
+        let changeColorStep: number = 15
+
+        if (this._level != null) {
+            for (let iterator: number = 0; iterator < this._level.items.length; iterator++) {
+                let bgColor: string = colorRed + colorGreen + colorBlue;
+                console.log("color = " + bgColor);
+                let bookmark: Bookmark = new Bookmark(
+                    this._level.items[iterator].mapX,
+                    this._level.items[iterator].mapY,
+                    () => {
+                        this.buttonClick(this._level.items[iterator].link);
+                    },
+                    parseInt(bgColor, 16));
+                this.addChild(bookmark);
+                bookmark.x = bookmarkX;
+                bookmark.y = bookmarkY;
+                bookmarkX += bookmark.width + Global.GAP;
+
+                if (bookmarkX + bookmark.width > Global.WINDOW_WIDTH) {
+                    bookmarkX = 0;
+                    bookmarkY += bookmark.height + Global.GAP;
+
+                    let testcolorBlue = Math.abs(Number(colorBlue));
+                    let changeBlue: number = testcolorBlue -= changeColorStep;
+                    colorBlue = changeBlue as any as string;
+
+                    let testcolorRed = Math.abs(Number(colorRed));
+                    let changeRed: number = testcolorRed += changeColorStep;
+                    colorRed = changeRed as any as string;
+                }
+            }
+        }
+    }
+
+    private buttonClick(link:string):void {
+        console.log("click");
+        window.open(link, '_blank')
+    }
+}
--- src/Main_Container.ts
+++ src/Main_Container.ts
@@ -1,14 +1,14 @@
+import Bookmark_Grid from "./Bookmark_Grid";
 import Background from "./Background";
-import Bookmark from "./Bookmark";
 import Global from "./Global";
 import Container = PIXI.Container;
 import "pixi.js";
 
 export default class Main_Container extends Container {
 	public static JSON_LOADER:XMLHttpRequest;
-	private _bookmarkContainer:PIXI.Container;
 	private _level:ILevel;
-
+	private _background:Background;
+	private _bookmarkGrid:Bookmark_Grid;
 	constructor() {
 		super();
 		this.jsonLoader();
@@ -17,7 +17,6 @@ export default class Main_Container extends Container {
 	private jsonLoader():void {
 		Main_Container.JSON_LOADER = new XMLHttpRequest();
 		Main_Container.JSON_LOADER.responseType = "json";
-
 		Main_Container.JSON_LOADER.open("GET", "base.json", true);
 		Main_Container.JSON_LOADER.onreadystatechange = () => {
 			this._level = Main_Container.JSON_LOADER.response;
@@ -39,58 +38,14 @@ export default class Main_Container extends Container {
 	}
 
 	private createBackground():void {
-		let background = new Background();
-		this.addChild(background);
+		this._background = new Background();
+		this.addChild(this._background);
 	}
 
 	private createBookmarks():void {
-		if (! this._bookmarkContainer) {
-			this._bookmarkContainer = new PIXI.Container;
-			this._bookmarkContainer.x = Global.GAP * 2;
-			this._bookmarkContainer.y = Global.GAP * 5.5;
-			this.addChild(this._bookmarkContainer);
-		}
-
-		let bookmarkX:number = 0;
-		let bookmarkY:number = 0;
-		let colorRed:string = "10";
-		let colorGreen:string = "50";
-		let colorBlue:string = "90";
-		let changeColorStep:number = 15
-
-		if (this._level != null) {
-			for (let iterator:number = 0; iterator < this._level.items.length; iterator++) {
-				let bgColor:string = colorRed + colorGreen + colorBlue;
-				console.log("color = " + bgColor);
-				let bookmark:Bookmark = new Bookmark(
-					this._level.items[iterator].mapX,
-					this._level.items[iterator].mapY,
-					()=>{this.buttonClick(this._level.items[iterator].link);},
-					parseInt(bgColor, 16));
-				this._bookmarkContainer.addChild(bookmark);
-				bookmark.x = bookmarkX;
-				bookmark.y = bookmarkY;
-				bookmarkX += bookmark.width + Global.GAP;
-
-				if (bookmarkX + bookmark.width > Global.WINDOW_WIDTH) {
-					bookmarkX = 0;
-					bookmarkY += bookmark.height + Global.GAP;
-
-					let testcolorBlue = Math.abs(Number(colorBlue));
-					let changeBlue:number = testcolorBlue -= changeColorStep;
-					colorBlue = changeBlue as any as string;
-
-					let testcolorRed = Math.abs(Number(colorRed));
-					let changeRed:number = testcolorRed += changeColorStep;
-					colorRed = changeRed as any as string;
-				}
-			}
-		}
-		this._bookmarkContainer.x = (Global.WINDOW_WIDTH - this._bookmarkContainer.width) / 2;
-	}
-
-	private buttonClick(link:string):void {
-		console.log("click");
-		window.open(link, '_blank')
+		this._bookmarkGrid = new Bookmark_Grid(Main_Container.JSON_LOADER.response);
+		this._bookmarkGrid.x = (Global.WINDOW_WIDTH - this._bookmarkGrid.width) / 2;
+		this._bookmarkGrid.y = Global.GAP * 5;
+		this.addChild(this._bookmarkGrid);
 	}
 }
